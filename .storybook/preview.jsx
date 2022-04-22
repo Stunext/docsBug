2ee0fbe72fa6ee4ca74ab54b518fac7754d5dd88
Andres Fuentes <andres@teachlr.org>
2022-04-22 12:23:23 -0300

    Changes to demonstrate the docs tab error

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,3 +1,5 @@
+import React from "react"
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +9,10 @@ export const parameters = {
     },
   },
 }
+
+export const decorators = [
+  (Story) => {
+    console.log(React.version);
+    return <Story />;
+  }
+];
