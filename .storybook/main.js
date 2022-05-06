0fcc4ee017364404e9ec3d13ec86a313964301ad
Andres Fuentes <andres@teachlr.org>
2022-05-06 10:04:08 -0300

    Update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,12 @@
 module.exports = {
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-a11y"],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-a11y"],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite"
   },
-  "reactOptions": { "legacyRootApi": false },
+  reactOptions: { legacyRootApi: false },
+  features: {
+    previewMdx2: true,
+  },
 };
